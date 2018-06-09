92789e0fe061ecad365916c23eae8dfd5f910f10
Jonas <jonas.lange@hotmail.de>
2018-06-09 16:23:28 +0200

    libs eingebunden update

--- LayerAssignmentProject/src/src/tdojlafry/layered/layerAssignment/graphData/LayerAssignment.java
+++ LayerAssignmentProject/src/src/tdojlafry/layered/layerAssignment/graphData/LayerAssignment.java
@@ -1,13 +1,14 @@
 package src.tdojlafry.layered.layerAssignment.graphData;
 
 import org.eclipse.elk.graph.ElkNode;
+import java.util.*;
 
 public class LayerAssignment {
 
-    public static MyGraph assignLayers(ElkNode layoutGraph) {
+    public static List<MyGraph> assignLayers(ElkNode layoutGraph) {
         
         
-        return new MyGraph();
+        return new ArrayList<MyGraph>();
     }
     
 }
