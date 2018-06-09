4da42b1631b58d4677d56bd9168b9420e598138e
Jonas <jonas.lange@hotmail.de>
2018-06-09 14:54:27 +0200

    LayerAssingnment added

---
+++ LayerAssignmentProject/src/src/tdojlafry/layered/layerAssignment/graphData/LayerAssignment.java
@@ -0,0 +1,10 @@
+package src.tdojlafry.layered.layerAssignment.graphData;
+
+
+public class LayerAssignment {
+
+    public static MyGraph assignLayers() {
+        return new MyGraph();
+    }
+    
+}
